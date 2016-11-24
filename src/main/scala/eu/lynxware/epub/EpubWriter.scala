541624fa335d3a8c6693f22ee100d17b23cc5898
Tymoteusz Kubicz <tymoteusz.kubicz@gmail.com>
2016-11-24 18:06:14 +0100

    #1 - first approach to refactoring of packToZipFile function.

--- src/main/scala/eu/lynxware/epub/EpubWriter.scala
+++ src/main/scala/eu/lynxware/epub/EpubWriter.scala
@@ -5,11 +5,18 @@ import java.nio.file.{Files, Path}
 import java.util.zip.{ZipEntry, ZipOutputStream}
 
 import com.typesafe.scalalogging.LazyLogging
+import eu.lynxware.epub.file.OpfManifestItemMediaType.OpfManifestItemMediaType
 import eu.lynxware.epub.file._
 
 import scala.xml.PrettyPrinter
 
 class EpubWriter extends LazyLogging {
+
+  private val defaultResourceLocation: Map[OpfManifestItemMediaType, String] = Map(
+    OpfManifestItemMediaType.ImageJpeg -> "img/",
+    OpfManifestItemMediaType.TextCss -> "css/",
+    OpfManifestItemMediaType.ApplicationXhtmlXml, "xhtml/"
+  )
 
   def write(book: Epub, output: Path): Unit = {
     packToZipFile(book, output)
@@ -29,49 +36,42 @@ class EpubWriter extends LazyLogging {
     zos.write(ContainerFile().toXml().toString.getBytes)
     zos.closeEntry()
 
-    val images = book.resources
-      .filter(_.mediaType == OpfManifestItemMediaType.ImageJpeg)
-      .map(r => OpfManifestItem("img/" + r.path.getFileName.toString, r.id, r.mediaType))
+    val images = filterImage(book)
+    val css = filterCss(book)
+    val content = filterContent(book)
 
-    book.resources
-      .filter(_.mediaType == OpfManifestItemMediaType.ImageJpeg)
-      .foreach { img =>
-        zos.putNextEntry(new ZipEntry(s"EPUB/img/" + img.path.getFileName.toString))
-        Files.copy(img.path, zos)
-        zos.closeEntry()
-      }
+    addManifestItemsToZipStream(zos, images ++ css ++ content)
 
-    val csses = book.resources
-      .filter(_.mediaType == OpfManifestItemMediaType.TextCss)
-      .map(r => OpfManifestItem("EPUB/css/" + r.path.getFileName.toString, r.id, r.mediaType))
-
-    book.resources
-      .filter(_.mediaType == OpfManifestItemMediaType.ApplicationXhtmlXml)
-      .foreach { content =>
-        zos.putNextEntry(new ZipEntry(s"EPUB/xhtml/" + content.path.getFileName.toString))
-        Files.copy(content.path, zos)
-        zos.closeEntry()
-      }
-
-    val content = book.resources
-      .filter(_.mediaType == OpfManifestItemMediaType.ApplicationXhtmlXml)
-      .map(r => OpfManifestItem("xhtml/" + r.path.getFileName.toString, r.id, r.mediaType, r.property))
-
-    // Spine items
-    val spineItems = book.resources.filter(_.isSpine).map(r => OpfSpineItem(r.id, None))
-
-    val manifestItems = images ++ csses ++ content
-    val opfFile = OpfFile()
-      .withMetadata(book.metadata)
-      .withManifestItems(manifestItems)
-      .withSpineItems(spineItems)
+    val spineItems = filterSpine(book)
+    val manifestItems = (images ++ css ++ content).map(_._1)
+    val opfFile = buildOpfFile(book.metadata, manifestItems, spineItems)
 
     val pp = new PrettyPrinter(120, 2)
-
     zos.putNextEntry(new ZipEntry("EPUB/package.opf"))
     zos.write(pp.formatNodes(opfFile.toXml()).getBytes)
     zos.closeEntry()
 
     zos.close()
   }
+
+  private def filterMediaType(book: Epub, mediaType: OpfManifestItemMediaType, folder: String) = book.resources
+    .filter(_.mediaType == mediaType)
+    .map(r => (OpfManifestItem(folder + r.path.getFileName.toString, r.id, r.mediaType), r))
+
+  private def filterImage(book: Epub) = filterMediaType(book, OpfManifestItemMediaType.ImageJpeg, defaultResourceLocation(OpfManifestItemMediaType.ImageJpeg))
+  private def filterCss(book: Epub) = filterMediaType(book, OpfManifestItemMediaType.TextCss, defaultResourceLocation(OpfManifestItemMediaType.TextCss))
+  private def filterContent(book: Epub) = filterMediaType(book, OpfManifestItemMediaType.ApplicationXhtmlXml, defaultResourceLocation(OpfManifestItemMediaType.ApplicationXhtmlXml))
+  private def filterSpine(book: Epub): Seq[OpfSpineItem] = book.resources.filter(_.isSpine).map(r => OpfSpineItem(r.id, None))
+
+  private def addManifestItemsToZipStream(zos: ZipOutputStream, items: Seq[(OpfManifestItem, Resource)]): Unit =
+    items.foreach(item => addNextEntry(zos, item._2))
+
+  private def addNextEntry(zos: ZipOutputStream, resource: Resource): Unit = {
+    zos.putNextEntry(new ZipEntry(s"EPUB/" + defaultResourceLocation.get(resource.mediaType) + resource.path.getFileName.toString))
+    Files.copy(resource.path, zos)
+    zos.closeEntry()
+  }
+
+  private def buildOpfFile(metadata: OpfMetadata, manifestItems: Seq[OpfManifestItem], spineItems: Seq[OpfSpineItem]): OpfFile =
+    OpfFile().withMetadata(metadata).withManifestItems(manifestItems).withSpineItems(spineItems)
 }
