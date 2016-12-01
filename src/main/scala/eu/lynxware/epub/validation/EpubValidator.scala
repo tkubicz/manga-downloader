28eb2573b37af96908cf784c78e5591baaea721d
Tymoteusz Kubicz <tymoteusz.kubicz@gmail.com>
2016-12-01 18:50:47 +0100

    #5 - Validation of local anchors added.

--- src/main/scala/eu/lynxware/epub/validation/EpubValidator.scala
+++ src/main/scala/eu/lynxware/epub/validation/EpubValidator.scala
@@ -12,17 +12,11 @@ object EpubValidator {
   private val atLeastOneSpineItemNeeded: Epub => (Boolean, String) =
     e => (e.resources.exists(r => r.isSpine), "At least one spine item is needed")
 
-  private val validators = Seq(exactlyOneNavNeeded, atLeastOneSpineItemNeeded)
+  private val linksAreValid: Epub => (Boolean, String) = validateLinks
+
+  private val validators = Seq(exactlyOneNavNeeded, atLeastOneSpineItemNeeded, linksAreValid)
 
   def validate(epub: Epub): (Boolean, Seq[String]) = {
-
-    val test = findAllLinks _ andThen filterOutLinksWithProtocols andThen filterOutLinksToTheSameDocument
-
-    test(epub).foreach { r =>
-      println(r.resource)
-      r.links.foreach(println)
-    }
-
     val messages = validators.map(v => v(epub)).filter(!_._1).map(_._2)
     if (messages.nonEmpty) (false, messages)
     else (true, Seq.empty)
--- src/main/scala/eu/lynxware/epub/validation/LinkValidation.scala
+++ src/main/scala/eu/lynxware/epub/validation/LinkValidation.scala
@@ -5,24 +5,48 @@ import eu.lynxware.epub.{Epub, Resource}
 
 import scala.io.Source
 
-case class ResourceWithLinks(resource: Resource, links: Seq[String])
+case class ResourceWithLinks(resource: Resource, links: Seq[String] = Seq.empty, anchors: Seq[String] = Seq.empty)
 
 private object LinkValidation {
   private val linkRegexp = "(?:href=\")([^\\\"\\r\\n]+)".r
+  private val nameOrIdRegexp = "(?:name=\"|id=\")([^\\\"\\r\\n]+)".r
   private val protocols = Seq("http://", "https://", "ftp://", "mailto:")
 
-  private[validation] def findAllLinks(epub: Epub): Seq[ResourceWithLinks] = epub.resources
-    .filter(_.mediaType == MediaTypes.Application.XhtmlXml)
-    .map(r => ResourceWithLinks(r, findAllLinksUsingRegexp(r)))
+  // TODO: Change result type to return also messages
+  private[validation] def validateLinks(epub: Epub): (Boolean, String) = {
+    val findInterestingLinks = findAllLinks _ andThen filterOutLinksWithProtocols
+    val localAnchorLinks = findInterestingLinks andThen filterLocalAnchorLinks
 
-  private[validation] def findAllLinksUsingRegexp(resource: Resource): Seq[String] =
+    val res = localAnchorLinks(epub).map(r => findInvalidLocalAnchorLinks(r.links, r.anchors))
+    val messages = res.flatten
+
+    if (messages.isEmpty) (true, "")
+    else (false, messages.mkString("\n"))
+  }
+
+  private def findAllLinks(epub: Epub): Seq[ResourceWithLinks] = epub.resources
+    .filter(_.mediaType == MediaTypes.Application.XhtmlXml)
+    .map(r => ResourceWithLinks(r, findAllLinksUsingRegexp(r), findAllAnchorsUsingRegexp(r)))
+
+  private def findAllLinksUsingRegexp(resource: Resource): Seq[String] =
     linkRegexp.findAllIn(Source.fromFile(resource.path.toFile).getLines().mkString).matchData.map(_.group(1)).toSeq
 
-  private[validation] def filterOutLinksWithProtocols(resourcesWithLinks: Seq[ResourceWithLinks]): Seq[ResourceWithLinks] =
+  private def findAllAnchorsUsingRegexp(resource: Resource): Seq[String] =
+    nameOrIdRegexp.findAllIn(Source.fromFile(resource.path.toFile).getLines().mkString).matchData.map(_.group(1)).toSeq
+
+  private def filterOutLinksWithProtocols(resourcesWithLinks: Seq[ResourceWithLinks]): Seq[ResourceWithLinks] =
     resourcesWithLinks.map(r => r.copy(links = r.links.filterNot(isExternalLink)))
 
-  private[validation] def isExternalLink(link: String): Boolean = protocols.exists(p => link.startsWith(p))
+  private def filterLocalAnchorLinks(resourceWithLinks: Seq[ResourceWithLinks]): Seq[ResourceWithLinks] =
+    resourceWithLinks.map(r => r.copy(links = r.links.filter(isLocalAnchorLink).map(_.drop(1))))
 
-  private[validation] def filterOutLinksToTheSameDocument(resourcesWithLinks: Seq[ResourceWithLinks]) =
-    resourcesWithLinks.map(r => r.copy(links = r.links.filterNot(_.startsWith("#"))))
+  private def filterOutLocalAnchorLinks(resourcesWithLinks: Seq[ResourceWithLinks]): Seq[ResourceWithLinks] =
+    resourcesWithLinks.map(r => r.copy(links = r.links.filterNot(isLocalAnchorLink)))
+
+  private def findInvalidLocalAnchorLinks(links: Seq[String], anchors: Seq[String]): Seq[String] =
+    links.filterNot(anchors.toSet)
+
+  private def isExternalLink(link: String): Boolean = protocols.exists(p => link.startsWith(p))
+
+  private def isLocalAnchorLink(link: String): Boolean = link.startsWith("#")
 }
